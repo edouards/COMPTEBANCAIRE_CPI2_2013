ad24cc1a07dd09c5b258501755f1e85f63ba7a02
edouard souan <e.souanmarcelon@epsi.fr>
2013-03-19 12:30:07 +0100

    UPDATE(19/03/2013)
    ajout table client

--- scriptBDD.sql
+++ scriptBDD.sql
@@ -4,4 +4,9 @@
  USE COMPTEBANCAIRE;
  
  //Création des tables
+ CREATE TABLE client(
+ cli_id INT NOT NULL auto_increment PRIMARY KEY,
+ cli_nom VARCHAR[25] NOT NULL,
+ cli_prenom VARCHAR[25] NOT NULL
+ );
  
