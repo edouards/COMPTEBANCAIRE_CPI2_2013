de9f06bceb7b2891feb13cd8aed9df07a46218c1
edouard souan <e.souanmarcelon@epsi.fr>
2013-03-26 11:48:03 +0100

    UPDATE 26/03/13 3
    Ajout posseder1

--- scriptBDD.sql
+++ scriptBDD.sql
@@ -19,6 +19,14 @@ moy_id INT NOT NULL PRIMARY KEY auto_increment,
 moy_libelle VARCHAR(25) NOT NULL
 );
 
+CREATE TABLE posseder1(
+pos1_id INT NOT NULL PRIMARY KEY auto_increment,
+pos1_idcli INT,
+pos1_cptNum INT,
+FOREIGN KEY FK_POS1_CLI(pos1_idCli) REFERENCES client(cli_id),
+FOREIGN KEY FK_POS1_CPT(pos1_cptNum) REFERENCES compte(com_numero)
+);
+
 CREATE TABLE possede2(
 poss2_moyId INT NOT NULL,
 poss2_numeroCompte INT NOT NULL
