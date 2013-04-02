d11421e9666f4d059832ed712d145c11c1891c5b
edouard souan <e.souanmarcelon@epsi.fr>
2013-04-02 11:29:44 +0200

    Data BDD
    Données minimum pour travailler ;)

--- data.sql
+++ data.sql
@@ -1,4 +1,23 @@
 INSERT INTO client(cli_nom,cli_prenom)
  VALUES('SOUAN','EDOUARD');
- INSERT INTO client(cli_nom,cli_prenom)
+INSERT INTO client(cli_nom,cli_prenom)
  VALUES('MACHUT','NICOLAS');
+ 
+INSERT INTO typeCompte(typeC_libelle)
+ VALUES('COURANT');
+INSERT INTO typeCompte(typeC_libelle)
+ VALUES('CHEQUE');
+INSERT INTO typeCompte(typeC_libelle)
+ VALUES('P.E.L');
+
+INSERT INTO moyenPaiement(moy_libelle)
+ VALUES('CB');
+INSERT INTO moyenPaiement(moy_libelle)
+ VALUES('CHEQUE');
+INSERT INTO moyenPaiement(moy_libelle)
+ VALUES('LIQUIDE');
+INSERT INTO moyenPaiement(moy_libelle)
+ VALUES('VIREMENT');
+ 
+
+ 
