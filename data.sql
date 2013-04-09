3b58b1e9a99e961dda971b03cbacd4ad71c1d825
edouard souan <e.souanmarcelon@epsi.fr>
2013-04-09 11:57:52 +0200

    DATA opérationnelles
    09/04/13

--- data.sql
+++ data.sql
@@ -12,26 +12,44 @@ VALUES('COURANT'),
 ('P.E.L');
  
 INSERT INTO compte(com_numero, com_solde, com_dateCreation, com_idTypeCpt)
-VALUES('0685493486','150.00', '2012/03/23', 0),
-('6457934526','23000.00', '1994/02/12', 3),
-('2893746378', '-12.30', '2010/12/20', 0),
-('2907364537', '845.10', '2010/05/12', 2),
-('1029745364','220.00', '2005/10/10', 1),
-('4637846573', '12000,00', '2000/07/28', 0),
-('3849274839','33.10', '1985/12/12', 1),
-('4839274637', '437.39', '1999/03/15', 1),
-('6735467684','1245674.10','1992/04/23',0);
+VALUES('9679098764565',1000, current_date, (SELECT typeC_id FROM typeCompte WHERE typeC_libelle='COURANT'));
+ 
+INSERT INTO compte(com_numero, com_solde, com_dateCreation, com_idTypeCpt)
+VALUES('0639498564261',50, current_date, (SELECT typeC_id FROM typeCompte WHERE typeC_libelle='COURANT'));
 
+INSERT INTO compte(com_numero, com_solde, com_dateCreation, com_idTypeCpt)
+VALUES('0145368534041',400, current_date, (SELECT typeC_id FROM typeCompte WHERE typeC_libelle='COURANT'));
+ 
+INSERT INTO compte(com_numero, com_solde, com_dateCreation, com_idTypeCpt)
+VALUES('0685493486',150.00, '2012/03/23', 1),
+('6457934526',23000.00, '1994/02/12', 4),
+('2893746378', -12.30, '2010/12/20', 1),
+('2907364537', 845.10, '2010/05/12', 3),
+('1029745364',220.00, '2005/10/10', 2),
+('4637846573', 12000.00, '2000/07/28', 1),
+('3849274839',33.10, '1985/12/12', 2),
+('4839274637', 437.39, '1999/03/15', 2),
+('6735467684',1245674.10,'1992/04/23',1);
+
+INSERT INTO posseder1(pos1_idcli,pos1_comNum)
+VALUES((SELECT cli_id FROM client WHERE cli_nom='MACHUT'),'9679098764565');
+ 
+INSERT INTO posseder1(pos1_idcli,pos1_comNum)
+VALUES((SELECT cli_id FROM client WHERE cli_nom='SOUAN'),'0639498564261');
+
+INSERT INTO posseder1(pos1_idcli,pos1_comNum)
+VALUES((SELECT cli_id FROM client WHERE cli_nom='SOUAN'),'0145368534041');
+ 
 INSERT INTO posseder1(pos1_idCli, pos1_comNum)
-VALUES('0', '0685493486'),
-('0','6457934526'),
-('1','2893746378'),
-('1','2907364537'),
-('2','1029745364'),
-('2','4839274637'),
-('3','4637846573'),
-('3','3849274839'),
-('4','6735467684');
+VALUES(1, '0685493486'),
+(1,'6457934526'),
+(1,'2893746378'),
+(1,'2907364537'),
+(3,'1029745364'),
+(3,'4839274637'),
+(4,'4637846573'),
+(4,'3849274839'),
+(5,'6735467684');
 
 INSERT INTO moyenPaiement(moy_libelle)
 VALUES('CB'),
@@ -41,16 +59,16 @@ VALUES('CB'),
 ('VIREMENT');
 
 INSERT INTO posseder2(pos2_moyId,pos2_comNum)
- VALUES((SELECT moy_id FROM moyenPaiement WHERE moy_libelle='CB'),
- (SELECT com_numero FROM compte INNER JOIN posseder1 ON com_numero=pos1_comNum INNER JOIN client 
- ON cli_id=pos1_idcli WHERE cli_nom='MACHUT'));
+VALUES((SELECT moy_id FROM moyenPaiement WHERE moy_libelle='CB'),
+(SELECT com_numero FROM compte INNER JOIN posseder1 ON com_numero=pos1_comNum INNER JOIN client 
+ON cli_id=pos1_idcli WHERE cli_nom='MACHUT'));
  
- INSERT INTO posseder2(pos2_moyId,pos2_comNum)
- VALUES((SELECT moy_id FROM moyenPaiement WHERE moy_libelle='CHEQUE'),
- (SELECT com_numero FROM compte INNER JOIN posseder1 ON com_numero=pos1_comNum INNER JOIN client 
- ON cli_id=pos1_idcli WHERE cli_nom='MACHUT'));
+INSERT INTO posseder2(pos2_moyId,pos2_comNum)
+VALUES((SELECT moy_id FROM moyenPaiement WHERE moy_libelle='CHEQUE'),
+(SELECT com_numero FROM compte INNER JOIN posseder1 ON com_numero=pos1_comNum INNER JOIN client 
+ON cli_id=pos1_idcli WHERE cli_nom='MACHUT'));
  
- INSERT INTO typeOperation(typeO_libelle)
+INSERT INTO typeOperation(typeO_libelle)
 VALUES('Debits'),
 ('Credits'),
 ('Cheque emis'),
@@ -73,13 +91,17 @@ VALUES('Alimentaire'),
 ('Autre');
 
 INSERT INTO operations(ope_date, ope_montant, ope_recu, ope_idMoy, ope_idCat, ope_idType)
-VALUES('2012/09/11','120.00', '', '0', '0', '6'),
-('2013/01/03' , '17.25', '', '1', '8', '3'),
-('2005/04/02' , '550.00', '', '4', '3', '5'),
-('2013/01/05', '600.00', '', '2', '5', '8'),
-('2007/10/18', '25.10', '', '3', '0', '6');
+VALUES('2012/09/11','120.00', '', 1, 1, 7);
 
-INSERT INTO effectue()
-VALUES();
+INSERT INTO operations (ope_date, ope_montant, ope_idMoy, ope_idCat, ope_idType)
+VALUES("2013/04/01", "150.00", 4,6,9 );
 
+INSERT INTO operations(ope_date, ope_montant, ope_recu, ope_idMoy, ope_idCat, ope_idType)
+VALUES('2012/09/11','120.00', '', 1, 1, 6),
+('2013/01/03' , '17.25', '', 2, 9, 3),
+('2005/04/02' , '550.00', '', 5, 4, 5),
+('2013/01/05', '600.00', '', 3, 6, 8),
+('2007/10/18', '25.10', '', 4, 1, 6);
 
+INSERT INTO effectue(eff_comNum, eff_opId)
+VALUES('6457934526', 2);
